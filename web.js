762a41bf083e7f1cbdc88aeeb78d9414665b087e
Eduardo Garcia <surfer.addict@gmail.com>
2013-07-08 17:23:42 +0000

    Updated web.js with fs

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(20);
+
+buffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf8', 0, buffer.length));
 });
 
 var port = process.env.PORT || 5000;
